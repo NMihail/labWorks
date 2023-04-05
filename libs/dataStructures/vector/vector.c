24d6f3718bac26ac78f6bcc4271f001f1da8df43
NMihail <nmihail535@gmail.com>
2023-04-05 17:24:49 +0300

    create files

---
+++ libs/dataStructures/vector/vector.c
@@ -0,0 +1,5 @@
+//
+// Created by Worker on 05.04.2023.
+//
+
+#include "vector.h"
---
+++ libs/dataStructures/vector/vector.h
@@ -0,0 +1,8 @@
+//
+// Created by Worker on 05.04.2023.
+//
+
+#ifndef VECTORS_VECTOR_H
+#define VECTORS_VECTOR_H
+
+#endif //VECTORS_VECTOR_H
